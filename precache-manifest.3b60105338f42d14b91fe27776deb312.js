7e881a75ece9d78010adbed0fa31edb65280f7d1
devarshitechnotion <devarsh.patel@itechnotion.in>
2018-12-24 09:44:48 +0530

    Updates

---
+++ precache-manifest.3b60105338f42d14b91fe27776deb312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05f9f01e3b54b5245ef4",
+    "url": "/reactdemo/static/js/runtime~main.05f9f01e.js"
+  },
+  {
+    "revision": "de95458f011508a63677",
+    "url": "/reactdemo/static/js/main.de95458f.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/reactdemo/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "de95458f011508a63677",
+    "url": "/reactdemo/static/css/main.3310ea1c.chunk.css"
+  },
+  {
+    "revision": "37a7b9876a1c57569aeb25d6b6d095e1",
+    "url": "/reactdemo/index.html"
+  }
+];
